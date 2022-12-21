e3889ab6b569c8aa209547de591ccf4471578373
lamtran415 <austin415@ymail.com>
2022-12-21 14:25:18 -0800

    Refactored Add Image + Edit A Spot & Completed Delete a Spot

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -11,9 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Review.belongsTo(models.Spot, { foreignKey: "spotId"})
-      Review.belongsTo(models.User, { foreignKey: "userId"})
-      Review.hasMany(models.ReviewImage, { foreignKey: "reviewId" });
+      Review.belongsTo(models.Spot, { foreignKey: "spotId", onDelete: "CASCADE"})
+      Review.belongsTo(models.User, { foreignKey: "userId", onDelete: "CASCADE"})
+      Review.hasMany(models.ReviewImage, { foreignKey: "reviewId", onDelete: "CASCADE"});
     }
   }
   Review.init({
