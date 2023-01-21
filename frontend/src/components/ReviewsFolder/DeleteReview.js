2b8738b18ba0f0c69d883c80f44eaf84e68f549e
lamtran415 <austin415@ymail.com>
2023-01-20 19:51:32 -0800

    Delete Spot, found a better solution

--- frontend/src/components/ReviewsFolder/DeleteReview.js
+++ frontend/src/components/ReviewsFolder/DeleteReview.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { thunkDeleteReview } from "../../store/reviewReducer";
 import { thunkLoadOneSpot } from "../../store/spotReducer";
