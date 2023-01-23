b9112bb578a700cb50535183eca41de622dbfc6b
lamtran415 <austin415@ymail.com>
2023-01-22 22:31:47 -0800

    Fixing some bugs

--- frontend/src/components/Spots/DeleteSpot.js
+++ frontend/src/components/Spots/DeleteSpot.js
@@ -1,12 +1,17 @@
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { thunkDeleteSpot } from "../../store/spotReducer";
+import { thunkLoadAllSpots } from "../../store/spotReducer";
 
 const DeleteSpot = () => {
     const dispatch = useDispatch();
     const history = useHistory();
     const { spotId } = useParams();
 
+    useEffect(() => {
+        dispatch(thunkLoadAllSpots)
+    },[])
 
     const handleClick = () => {
         dispatch(thunkDeleteSpot(spotId))
