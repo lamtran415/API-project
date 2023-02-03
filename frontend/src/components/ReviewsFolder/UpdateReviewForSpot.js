6cd2d390e94729715dffdf42c400102e99eb94d0
lamtran415 <austin415@ymail.com>
2023-02-03 15:11:56 -0800

    Finish Updating Review for Spot

--- frontend/src/components/ReviewsFolder/UpdateReviewForSpot.js
+++ frontend/src/components/ReviewsFolder/UpdateReviewForSpot.js
@@ -1,64 +1,46 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux"
 import { useHistory } from "react-router-dom";
 import { useModal } from "../../context/Modal";
-import { thunkCreateReviewForSpot } from "../../store/reviewReducer";
+import { thunkLoadReviewsForSpot, thunkUpdateReviewForSpot } from "../../store/reviewReducer";
 import { thunkLoadOneSpot } from "../../store/spotReducer";
-import './CreateReviewForSpot.css'
 
-const CreateReviewForSpots = ({spotId, copySessionUser }) => {
+const UpdateReviewForSpot = ({review}) => {
     const dispatch = useDispatch();
     const history = useHistory();
-    const [review, setReview] = useState("");
-    const [stars, setStars] = useState(1);
+    const [editReview, setEditReview] = useState(review.review);
+    const [stars, setStars] = useState(review.stars);
     const [errors, setErrors] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
-    const [isButtonDisabled, setIsButtonDisabled] = useState(false)
     const { closeModal } = useModal();
-    const userObj = {User: {...copySessionUser.user}}
-    let reviews = useSelector((state) => state.reviews);
-    let reviewsArr = Object.values(reviews)
-
-
-    useEffect(() => {
-        const newErrors = [];
-        reviewsArr.forEach(review => {
-            if (review.User.id === copySessionUser.user.id) {
-                newErrors.push(<li className="cannot-submit-text">Cannot submit another review</li>);
-                setIsButtonDisabled(true)
-            }
-        })
-        setErrors(newErrors)
-    }, [])
-
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        setErrors([]);
+        e.preventDefault()
+        setErrors([])
 
         const reviewDetails = {
-            review,
+            id: review.id,
+            review: editReview,
             stars
-        };
+        }
 
-        return await dispatch(thunkCreateReviewForSpot(reviewDetails, spotId, userObj))
-            .then(() => history.push(`/spots/${spotId}`))
+        return await dispatch(thunkUpdateReviewForSpot(reviewDetails, review.id))
+            .then(() => history.push(`/spots/${review.spotId}`))
             .then(setIsLoaded(true))
             .then(() => closeModal())
             .catch(async (res) => {
                 const data = await res.json();
                 if (data && data.errors) setErrors(data.errors)
             });
-
     }
 
     useEffect(() => {
-        dispatch(thunkLoadOneSpot(spotId))
-        setIsLoaded(false);
+        dispatch(thunkLoadOneSpot(review.spotId))
+        dispatch(thunkLoadReviewsForSpot(review.spotId))
+        setIsLoaded(false)
+    }, [dispatch, review.spotId, isLoaded])
 
-    }, [dispatch, spotId, isLoaded]);
-
-    return (
+    return(
         <div className="create-review-container">
             <h3 className="create-review-header"><div className="x-button" onClick={closeModal}>X</div><span>Leave Review</span></h3>
                 <form
@@ -74,9 +56,9 @@ const CreateReviewForSpots = ({spotId, copySessionUser }) => {
                         <input
                             type='text'
                             name='review'
-                            value={review}
+                            value={editReview}
                             placeholder='Enter a review'
-                            onChange={(e) => setReview(e.target.value)}
+                            onChange={(e) => setEditReview(e.target.value)}
                             required
                         />
                     </label>
@@ -91,11 +73,11 @@ const CreateReviewForSpots = ({spotId, copySessionUser }) => {
                             required
                         />
                     </label>
-                    <button disabled={isButtonDisabled} type="submit">Submit</button>
+                    <button type="submit">Submit</button>
                 </div>
             </form>
         </div>
     )
 }
 
-export default CreateReviewForSpots;
+export default UpdateReviewForSpot
