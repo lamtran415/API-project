4403ea465ec18e8fab3f1af7ca9632d6d0fc16b1
lamtran415 <austin415@ymail.com>
2023-03-31 16:25:35 -0700

    Change backend

--- backend/db/seeders/20221220063758-booking.js
+++ backend/db/seeders/20221220063758-booking.js
@@ -55,6 +55,60 @@ module.exports = {
         userId: 1,
         startDate: "2022-05-19",
         endDate: "2022-05-20"
+      },
+      {
+        spotId: 4,
+        userId: 2,
+        startDate: "2023-04-19",
+        endDate: "2023-04-25"
+      },
+      {
+        spotId: 5,
+        userId: 3,
+        startDate: "2023-05-25",
+        endDate: "2023-05-29"
+      },
+      {
+        spotId: 6,
+        userId: 2,
+        startDate: "2023-05-20",
+        endDate: "2023-05-23"
+      },
+      {
+        spotId: 7,
+        userId: 3,
+        startDate: "2023-06-26",
+        endDate: "2023-06-29"
+      },
+      {
+        spotId: 8,
+        userId: 1,
+        startDate: "2023-07-19",
+        endDate: "2023-07-25"
+      },
+      {
+        spotId: 9,
+        userId: 2,
+        startDate: "2023-10-19",
+        endDate: "2023-10-25"
+      },
+      {
+        spotId: 10,
+        userId: 3,
+        startDate: "2023-09-25",
+        endDate: "2023-09-28"
+      },
+      {
+        spotId: 11,
+        userId: 5,
+        startDate: "2023-06-19",
+        endDate: "2023-06-20"
+      },
+      {
+        spotId: 12,
+        userId: 4,
+        startDate: "2023-07-19",
+        endDate: "2023-07-20"
       }
     ])
   },
